4cf027819cd8e739f9666882edcea0f455f463bc
PGKM <preethangowdakm93@gmail.com>
2025-12-31 15:47:17 +0530

    Change sum calculation to use a loop
    Refactor sum calculation to use a loop instead of sum() function.

--- Control Structures in Python.py
+++ Control Structures in Python.py
@@ -17,11 +17,14 @@ else:
 #Task -2
 # Sum of integers from 1 to 50 using a loop
 
-for _ in range(1,51):
-    # Using range 1,51 to sum but number 51 is not include
-    total_sum = sum(range(1,51))
-    print(f"the sum of numbers from 1 to 50 is :{total_sum}")
-    break
+#variable for the sum
+total_count = 0
 
+#loop and sum of all integers in this range
+for number in range(1,51):
+    total_count += number
+
+#Displays the final sum
+print(f"The sum of numbers form 1 to 50 is:{total_count}")
 
 
